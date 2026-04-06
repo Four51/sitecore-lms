0fda15e42c1ac4fd4019587a874e46efe034904e
Kevin Ahn <keva@sitecore.net>
2026-04-06 15:40:39 -0500

    LMS Secure-4295 fix

--- src/Middleware/integrations/ordercloud.integrations.docebo/Models/DoceboToken.cs
+++ src/Middleware/integrations/ordercloud.integrations.docebo/Models/DoceboToken.cs
@@ -10,7 +10,6 @@ namespace ordercloud.integrations.docebo.Models
         public int expires_in { get; set; }
         public string token_type { get; set; }
         public string scope { get; set; }
-        public string refresh_token { get; set; }
     }
 
 }
